1d79ea5229856dad8fab060cc760f7ccd0239d48
Pritam <84720825+pritam12426@users.noreply.github.com>
2023-11-17 14:52:57 +0530

    Some more change
    Make Change In >>
                     - ./global/compile/main.c
                     - ./global/compile/makefile

--- global/compile/main.c
+++ global/compile/main.c
@@ -1,15 +1,122 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <sys/stat.h>
 
 
-bool is_char_in(char *_content, char _what){
-	for(int i = 0; ((_content[i]) != '\0'); i++){
-		if(_content[i] == _what) return true;
+void ch_str_lower(char *_content);
+bool str_ends_with(char *_content, char *_ends);
+void ch_str_replace(char *_content, const char _which, const char _what);
+
+
+int main(int argc, char *argv[]) {
+	if(argc != 2) return 2;
+
+	struct stat st;
+	if(stat(argv[1], &st) != 0){
+		printf("\033[1;31mFILE NOT FOUND\033[0m: { \033[1;36m%s\033[0m }\n", argv[1]);
+		return 1;
 	}
-	return false;
+
+	short int len_argv = (strlen(argv[1]) + 1);
+
+	if(str_ends_with(argv[1], ".py")){
+		char _command[len_argv + 9];
+
+		#ifdef linux
+			sprintf(_command, "%s %s", "python3", argv[1]);
+		#elif __APPLE__
+			sprintf(_command, "%s %s", "python3", argv[1]);
+		#elif __WIN32
+			sprintf(_command, "%s %s", "python", argv[1]);
+		#else
+			printf("\033[1;31mUNSUPPORTED SYSTEM\033[0m:(\n");
+			return 1;
+		#endif
+
+		return system(_command);
+	}
+
+	char *file_name;
+
+	if(strrchr(argv[1], '/') != NULL){
+		file_name = strrchr(argv[1], '/');
+	}
+	else {
+		file_name = &argv[1][0];
+	}
+
+	short int len_file_name = strlen(file_name) + 1;
+	char actual_file_name[len_file_name];
+	strcpy(actual_file_name, file_name);
+
+	short int return_value = -1;
+
+	char *temp_dir;
+
+	#ifdef linux
+		temp_dir = "/tmp/";
+	#elif __APPLE__
+		temp_dir = "~/Library/Caches/";
+	#elif __WIN32
+		temp_dir = "C:\\WINDOWS\\Temp\\";
+	#else
+		printf("\033[1;31mUNSUPPORTED SYSTEM\033[0m:(\n");
+		return 1;
+	#endif
+
+	short len_temp_dir = strlen(temp_dir);
+
+	char _command[len_argv + len_file_name + len_temp_dir + 14];
+
+	ch_str_lower(actual_file_name);
+	ch_str_replace(actual_file_name, '.', '-');
+
+	if(str_ends_with(argv[1], ".c")){
+		sprintf(_command, "gcc %s %s %s%s%s", argv[1], "-o", temp_dir, actual_file_name, ".out");
+		printf("%s\n", _command);
+		return_value = system(_command);
+	}
+	else if(str_ends_with(argv[1], ".cpp")){
+		sprintf(_command, "gpp %s %s %s%s%s", argv[1], "-o", temp_dir, actual_file_name, ".out");
+		printf("%s\n", _command);
+		return_value = system(_command);
+		
+	}
+	else{
+		printf("\033[1;31mUNSUPPORTED FILE TYPE\033[0m: { \033[1;36m%s\033[0m }.\n", file_name);
+		return 2;
+	}
+
+	if(return_value == 0){
+		char another_command[len_temp_dir + len_file_name];
+		sprintf(another_command, "%s%s.out", temp_dir, actual_file_name );
+		printf("%s\n\n", another_command);
+		return_value = system(another_command);
+	}
+
+	return return_value;
+}
+
+
+void ch_str_lower(char *_content){
+	for(int i = 0; ((_content[i]) != '\0'); i++){
+		_content[i] = tolower(_content[i]);
+	}
+}
+
+
+void ch_str_replace(char *_content, const char _which, const char _what){
+	int _len = strlen(_content);
+
+	for(_len; _len; _len--){
+		if(_content[_len] == _which){
+			_content[_len] = _what;
+			break;
+		} 
+	}
 }
 
 
@@ -28,85 +135,4 @@ bool str_ends_with(char *_content, char *_ends){
 	}
 
 	return true;
-}
-
-
-bool str_starts_with(char *_content, char *_starts){
-	int _len_starts = strlen(_starts);
-	int _len_content = strlen(_content);
-
-	if(_len_starts > _len_content){
-		return false;
-	}
-
-	for(int i = 0; i < _len_starts; i++){
-		if(_content[i] != _starts[i]) return false;
-	}
-
-	return true;
-}
-
-
-int main(int argc, char *argv[]) {
-	if(argc != 2) return 2;
-
-	struct stat st;
-	if(stat(argv[1], &st) != 0){
-		printf("\033[1;31mFILE NOT FOUND\033[0m: { \033[1;36m%s\033[0m }\n", argv[1]);
-		return 1;
-	}
-
-	short int af_path_len = (strlen(argv[1]) + 1);
-
-	if(str_ends_with(argv[1], ".py")){
-		char _command[af_path_len + 9];
-
-		#ifdef linux
-			sprintf(_command, "%s %s", "python3", argv[1]);
-		#elif __APPLE__
-			sprintf(_command, "%s %s", "python3", argv[1]);
-		#elif __WIN32
-			sprintf(_command, "%s %s", "python", argv[1]);
-		#else
-			printf("\033[1;31mUNSUPPORTED SYSTEM\033[0m:(\n");
-			return 1;
-		#endif
-
-		return system(_command);
-	}
-
-	char actual_file_path[af_path_len];
-	strcpy(actual_file_path, argv[1]);
-
-
-	char *file_name = strrchr(argv[1], '/') + 1;
-
-	short int _x = strlen(file_name);
-	char _command[af_path_len + _x + 19];
-	short int error_code = -1;
-
-	if(str_ends_with(argv[1], ".c")){
-		sprintf(_command, "%s %s %s %s%s%s", "gcc", argv[1], "-o", "/tmp/", file_name, ".out");
-		printf("%s\n", _command);
-		error_code = system(_command);
-	}
-	else if(str_ends_with(argv[1], ".cpp")){
-		sprintf(_command, "%s %s %s %s%s%s", "gpp", argv[1], "-o", "/tmp/", file_name, ".out");
-		printf("%s\n", _command);
-		error_code = system(_command);
-		
-	}
-	else{
-		printf("\033[1;31mUNSUPPORTED FILE TYPE\033[0m: { \033[1;36m%s\033[0m }.\n", file_name);
-		return 2;
-	}
-
-	if(error_code == 0){
-		char another_command[_x + 6];
-		sprintf(another_command, "/tmp/%s.out",file_name );
-		printf("%s\n", another_command);
-		return system(another_command);
-	}
-
-	return error_code;
 }
