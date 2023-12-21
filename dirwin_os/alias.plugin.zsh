775e92e63ce72aed9590073edf5e2bcfa152d72d
Pritam <84720825+pritam12426@users.noreply.github.com>
2023-12-21 22:08:24 +0530

    added a command to download the instal vdo
    Make Change In >>
                     - ./dirwin_os/alias.plugin.zsh

--- dirwin_os/alias.plugin.zsh
+++ dirwin_os/alias.plugin.zsh
@@ -25,12 +25,13 @@ alias tree="tre -ae"
 alias gfind="ls -aF --color=auto | grep -i"
 alias exiftool_f="exiftool -overwrite_original"
 alias rmdes="exiftool -overwrite_original -longdescription='' -description=''"
-alias yt-dlpp="yt-dlp -o '~/Downloads/yt_dlp/%(playlist)s/%(playlist_index)s-%(title)s.%(ext)s'"
+alias yt-dlpp="yt-dlp -o '~/Downloads/yt_dlp/%(playlist)s-%(uploader)s/%(playlist_index)s-%(title)s.%(ext)s'"
 alias yt-dlpwd="yt-dlp -o '%(title)s.%(ext)s'"
 alias gitconfig="git config user.name 'Pritam' && git config user.email 'pritamkumar12426@gmail.com'"
 alias scrcpy_r="scrcpy -m 1080 --max-fps=60 -r ~/Movies/screen_cast/scrcpy_$(date +'%Y-%m-%d_%H-%M-%S').mp4"
 alias envpath='echo; tr ":" "\n" <<< "$PATH"; echo;'
 alias githubconfig="git config user.name 'Pritam' && git config user.email '84720825+pritam12426@users.noreply.github.com'"
+alias insta="yt-dlp --embed-thumbnail --embed-metadata --ignore-config -o '~/Downloads/insta/%(channel)s-%(id)s_%(uploader_id)s.%(ext)s' -f 'bestvideo+bestaudio' --cookies-from-browser 'firefox'"
 
 alias song="ffplay -fs -ss 6741 -t 70 -loop -1 ~/Movies/movies/brahmastra_part_1.mkv >>/dev/null 2>&1"
 
