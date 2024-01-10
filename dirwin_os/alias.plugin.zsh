70f34926935d9aa615454d1ac5a6b74d57c4e7ec
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-01-10 22:06:27 +0530

    Make Change In >>
                     - ./dirwin_os/alias.plugin.zsh

--- dirwin_os/alias.plugin.zsh
+++ dirwin_os/alias.plugin.zsh
@@ -49,6 +49,7 @@ alias exf="exiftool -sort -P -overwrite_original_in_place"
 alias removeds="echo 'removed:' && findds && find . -name '.DS_Store' -exec rm -f {} +"
 alias rmdes="exiftool -P -overwrite_original_in_place -longdescription='' -description=''"
 alias scrcpy_r="scrcpy -r ~/Movies/screen_cast/scrcpy_$(date +'%Y-%m-%d_%H-%M-%S').mp4"
+alias vsdebug="curl 'https://raw.githubusercontent.com/pritam12426/systme-configuration/main/global/vscode/launch.json' -o launch.json"
 
 # yt-dlp
 alias yt-dlpwd="yt-dlp -o '%(title)s.%(ext)s'"
