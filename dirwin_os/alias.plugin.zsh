a19b1c389c0e0e73c26485cc1c0b5d7d66ae0633
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-01-24 20:53:46 +0530

    Make Change In >>
                     - ./dirwin_os/alias.plugin.zsh

--- dirwin_os/alias.plugin.zsh
+++ dirwin_os/alias.plugin.zsh
@@ -37,6 +37,15 @@ alias tdir="cd /Volumes/T7/main/videos/courses/"
 alias cpdir="cd ~/Developer/cpp_lang/practic"
 alias pdir="cd $present_working_project_directory"
 
+# color for man page
+export LESS_TERMCAP_mb=$'\e[1;32m'
+export LESS_TERMCAP_md=$'\e[1;32m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[01;33m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[1;4;31m'
+
 
 # System scripts
 alias lfind="alias | grep -i "
