ddd1054ece902101ab7bbc0e05ca94e75af3c2f5
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-02-16 09:14:11 +0530

    Make Change In >>
                     - ./dirwin_os/zshrc.sh
                     - ./dirwin_os/alias.plugin.zsh

--- dirwin_os/zshrc.sh
+++ dirwin_os/zshrc.sh
@@ -93,3 +93,5 @@ source $ZSH/oh-my-zsh.sh
 # You may need to manually set your language environment
 export LANG=en_US.UTF-8
 
+
+[[ "$TERM_PROGRAM" == "CodeEditApp_Terminal" ]] && . "/Applications/CodeEdit.app/Contents/Resources/codeedit_shell_integration.zsh"
