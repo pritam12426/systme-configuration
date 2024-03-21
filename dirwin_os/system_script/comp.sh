593e32d4324d5f3123931b0bc1905ebf278c1588
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-03-21 08:08:38 +0530

    Make Change In >>
                     - ./dirwin_os/system_script/comp.sh

--- dirwin_os/system_script/comp.sh
+++ dirwin_os/system_script/comp.sh
@@ -5,20 +5,12 @@
 if [[ $# -eq 0 ]]; then
 	if [ -f "main.c" ]; then
 		file="main.c"
-		echo "compiling main.c ..."
-
 	elif [ -f "main.cpp" ]; then
 		file="main.cpp"
-		echo "compiling main.cpp ..."
-
 	elif [ -f "main.py" ]; then
 		file="main.py"
-		echo "compiling main.py ..."
-
 	elif [ -f "main.rs" ]; then
 		file="main.rs"
-		echo "compiling main.rs ..."
-
 	else
 		echo "comp: missing operand"
 		exit 1
@@ -33,12 +25,14 @@ path=$(dirname "$file")
 
 # Checking file type.
 if [[ "$file" =~ .c$ ]]; then
-	GNU_version="-std=c2x"
+	# GNU_version="-std=c2x"
+	GNU_version="-std=gnu17"
 	command="clang"
 
 elif [[ "$file" =~ .cpp$ ]]; then
 	command="clang++"
-	GNU_version="-std=c++20"
+	# GNU_version="-std=c++20"
+	GNU_version="-std=gnu++2b"
 
 elif [[ "$file" =~ .rs$ ]]; then
 	cargo run
@@ -53,6 +47,7 @@ elif [[ "$file" =~ .html$ ]]; then
 	exit 0
 
 elif [[ "$file" =~ .py$ ]]; then
+	echo '>>' python3 $file
 	python3 "$file"
 	exit "$?"
 
@@ -72,6 +67,7 @@ else
 	root_data=$(cat "$file" | head -n 1 | cut -c 3-)
 
 	if [[ -f "$root_data" ]]; then
+		echo $root_data "$file"
 		$root_data "$file"
 		exit "$?"
 	else
@@ -82,13 +78,10 @@ fi
 
 
 filename=$(basename "$file")
-
-# new_name="${file%.*}"
 new_name="${filename//./-}"
 
-# Make bin dir if it is not present.
-
 # Run command for 'gcc' or 'gpp' according to the file type.
+echo -e "$command -Wall $GNU_version $filename"
 ("$command" -Wall "$GNU_version" "$path/$filename" -o "$TMPDIR$new_name.out")
 
 if [[ "$?" = 0 ]]; then
